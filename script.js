51e22dc5537ace3b62bc64a545a0b51ece0804cb
rodolfoscarp <rodolfoscarp@gmail.com>
2019-09-18 21:54:56 -0300

    Implementada todas as operações

--- script.js
+++ script.js
@@ -1,7 +1,14 @@
 var display = document.getElementById("display");
+var temp,op;
 
 function pressNumber(number){
-   display.value = display.value + number;
+    if (number!="0"){
+        display.value = display.value + number;
+    }
+    else if(display.value!=""){
+        display.value = display.value + number;
+    }
+   
 }
 
 function pressDec(){
@@ -17,6 +24,8 @@ function pressDec(){
 
 function pressCE(){
     display.value = "";
+    op=null;
+    temp=null;
 }
 
 function pressC(){
@@ -24,18 +33,43 @@ function pressC(){
 }
 
 function pressOP(op){
-    switch (op){
-        case "div":
-            break;
-        case "sum":
-            break;
-        case "sub":
-            break;
-        case "mult":
-            break;
+    if(this.op==null && display.value!=""){
+        this.op=op;
+        temp=Number(display.value);
+        display.value = "";
     }
 }
 
 function pressResult(){
-
+    if (display.value!="" && temp!=null){
+        switch (this.op){
+            case "sum":
+                temp+=Number(display.value);
+                display.value=temp;
+                this.op=null;
+                break;
+            case "sub":
+                temp-=Number(display.value);
+                display.value=temp;
+                this.op=null;
+                break;
+            case "div":
+                if (Number(display.value)!=0){
+                    temp=temp/(Number(display.value));
+                    display.value=temp;
+                    this.op=null;
+                }
+                else{
+                    temp=temp/(Number(display.value));
+                    display.value="Error";
+                    this.op=null;
+                }                
+                break;
+            case "mult":
+                temp=Number(display.value)*temp;
+                display.value=temp;
+                this.op=null;
+                break;
+        }
+    }
 }
